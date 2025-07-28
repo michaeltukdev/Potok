cd6c312f91052a55c9c0c2ccdaf25866a495d046
michaeltukdev <mdtilley04@gmail.com>
2025-07-28 14:42:14 +0100

    Check if vault exists

--- cmd/client/add-vault.go
+++ cmd/client/add-vault.go
@@ -3,13 +3,47 @@ package main
 import (
 	"fmt"
 
+	"github.com/michaeltukdev/Potok/internal/client"
 	"github.com/michaeltukdev/Potok/internal/config"
 	"github.com/michaeltukdev/Potok/internal/prompt"
 	"github.com/spf13/cobra"
+	"github.com/zalando/go-keyring"
 )
 
-func checkVault() {
-	fmt.Println("False")
+// TODO: Probably move from this file
+func VaultNameExists(vaults []client.Vault, name string) bool {
+	for _, v := range vaults {
+		if v.Name == name {
+			return true
+		}
+	}
+	return false
+}
+
+// TODO: Probably move from this file
+func checkVault(api, username, vaultName string) (bool, error) {
+	secret, err := keyring.Get("potok", "api-key")
+	if err != nil {
+		return false, fmt.Errorf("error retrieving API key: %w", err)
+	}
+
+	url := fmt.Sprintf("%s/users/%s/vaults", api, username)
+	resp, err := client.MakeAuthenticatedRequest(secret, url)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return false, fmt.Errorf("unauthenticated! Please set or update your API key")
+	}
+
+	vaults, err := client.ReadVaultsFromResponse(resp)
+	if err != nil {
+		return false, err
+	}
+
+	return VaultNameExists(vaults, vaultName), nil
 }
 
 var addVaultCmd = &cobra.Command{
@@ -22,17 +56,27 @@ var addVaultCmd = &cobra.Command{
 			return
 		}
 
-		fmt.Println(cfg)
-
 		fmt.Println("Adding Vault...")
 
 		vaultPath := prompt.Input("Path to your vault: ")
-		vaultName := prompt.Input("Name your vault: ")
-		checkVault()
 
-		vaultPassword := prompt.Input("Encryption password: ")
+		var vaultName string
+		for {
+			vaultName = prompt.Input("Name your vault: ")
+			exists, err := checkVault(cfg.APIURL, cfg.Username, vaultName)
+			if err != nil {
+				fmt.Println("Error checking vault:", err)
+				return
+			}
+			if exists {
+				fmt.Printf("A vault named '%s' already exists. Please choose a different name.\n", vaultName)
+			} else {
+				break
+			}
+		}
+
+		// vaultPassword := prompt.Input("Encryption password: ")
 
 		fmt.Printf("Vault Path: %s\nVault Name: %s\n", vaultPath, vaultName)
-		fmt.Println(vaultPassword)
 	},
 }
